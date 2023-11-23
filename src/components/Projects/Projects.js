e26f98787f5cd6b3e4938751ff9e104c4b705b90
RohitKumarSinha <sinharohit3333@gmail.com>
2023-11-23 17:27:42 +0530

    modify: project component

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -25,7 +25,7 @@ function Projects() {
               imgPath={GithubFinder}
               isBlog={false}
               title="GitHub-Finder"
-              description="This application fetches data from the GitHub Api. In this application, you type any person name and application shows you Github profile of that person"
+              description="This app helps you look at GitHub profiles easily! It uses HTML5, CSS, JavaScript, and React. With Axios, it gets information from GitHub without any hassle. Just type a name, and you'll see that person's GitHub profile right away."
               ghLink="https://github.com/RohitKumarSinha/react/tree/master/Github-finder"
               demoLink="https://githubfider33333.netlify.app/"
             />
@@ -36,7 +36,8 @@ function Projects() {
               imgPath={Ecommerce}
               isBlog={false}
               title="E-Commerce"
-              description="This is an E-commerce website where you signup, login, add product to cart, wish list your product. it also uses fake backend REST API. "
+              description="
+              This online shop is like a cool mall online! It’s made with HTML5, CSS, JavaScript, React, Redux, Axios, and Bootstrap. You can join up, log in, put stuff in your cart, and make a wish list of things you really like. But here's the fun part: you can do all this without buying anything for real! It's like playing in a store, but it's just for fun."
               ghLink="https://github.com/RohitKumarSinha/react/tree/master/E-Commerce"
               demoLink="https://shopanything.netlify.app"
             />
@@ -47,7 +48,7 @@ function Projects() {
               imgPath={Resturant}
               isBlog={false}
               title="Resturant Landing Page"
-              description="This is the beautiful landing page of the restaurant."
+              description="This restaurant's webpage looks awesome! It's made using HTML5, CSS, JavaScript, React, Axios, and Bootstrap. It's like a beautiful picture that shows you what the restaurant is like. You can explore and see what's there before actually going. It works well on phones too!"
               ghLink="https://github.com/RohitKumarSinha/react/tree/master/resturant"
               demoLink="https://resutrant-app.netlify.app/"              
             />
